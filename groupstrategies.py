eebfb3cee50da5d4b459620db552722e6a0fa6a0
Elijah Robinson <elijahqrobinson26@gosaints.org>
2025-02-11 21:12:13 -0600

    Added the program I created

--- groupstrategies.py
+++ groupstrategies.py
@@ -1,17 +1,25 @@
 # YOU ONLY HAVE TO SUBMIT FUNCTIONS FOR WHICH
 # YOU ARE THE DRIVER IN PAIR PROGRAMMING
-
+import random
 
 # Here are some history variables to test your code on. Feel free to create your own.
 hist1 = []
 hist2 = [("split","steal")]
-hist3 = [("split","split"),("steal","split"),("split,steal"),("split,split"),("steal,split")]
+hist3 = [("split","split"),("steal","split"),("split","steal"),("split","split"),("steal","split")]
 hist4 = [("split","steal"),("steal","steal"),("split","steal"),("steal","split"),("split","split"),("steal","split")]
 
 # Your team's 1st strategy (leave blank if you are not the driver)
-# Explanation of Strategy:
-# 
-
+# Explanation of Strategy: We are going to choose what the opponent chose last and the first choice will be random
+#
+def MarathonMavericksStrategy1(history):
+    if len(history) == 0:
+        choice = random.randint(0, 1)
+        if choice == 0:
+            return "split"
+        elif choice == 1:
+            return "steal"
+    else:
+        return history[len(history)-1][0]
 
 # Your team's 2nd strategy (leave blank if you are not the driver)
 # Explanation of Strategy:
